604075622abe486cb5e64ff2b24b5b3b07d72500
문정민 <moondeve41@gmail.com>
2023-04-11 15:37:07 +0900

    ADD :: 42627

---
+++ 3/42627/문정민.py
@@ -0,0 +1,27 @@
+import heapq
+import math
+def solution(jobs):
+    cnt, i = 0, 0
+    start, now = 0, 0
+    heap = []
+
+    while i < len(jobs):
+
+        for j in jobs:
+            if start <= j[0] <= now:
+                heapq.heappush(heap, [j[1], j[0]])
+
+        if heap:
+            cur = heapq.heappop(heap)
+            start = now
+            now += cur[0]
+            cnt += now - cur[1]
+            i += 1
+
+        else:
+            now += 1
+
+    return math.floor(cnt//len(jobs))
+
+
+print(solution([[0, 3], [1, 9], [2, 6]]))
